79d98765986e753a4b4fc0717d96cbe131bf9c7d
LHensman <lukehensman7@gmail.com>
2023-04-18 20:19:23 +0100

    listen and response

--- passer.js
+++ passer.js
@@ -1,6 +1,23 @@
-// standard express setup
-var express = require('express');
-var app = express();
+const express = require('express');
+const fs = require('fs');
 
-app.use(bodyParser.json());
+const app = express();
 
+app.get('/get_pass', (req, res) => {
+    console.log('Request received');
+  fs.readFile('pass.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const pass = data.trim().split('\n')[0];
+
+    res.send(pass);
+  });
+});
+
+app.listen(9090, () => {
+  console.log('Server started on http://localhost:9090');
+});
